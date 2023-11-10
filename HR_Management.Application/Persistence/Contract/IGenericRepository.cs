e9a259c72f60f1a2202435e62e5df73a54b0337c
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-10 09:02:11 +0330

    Add project files.

---
+++ HR_Management.Application/Persistence/Contract/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Persistence.Contract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(int id);
+
+        //فقط اطلاعات رو میخونه واکشی میکنه
+        Task<IReadOnlyList<T>> GetAll();
+        Task<T> Add(T entity);
+        Task<T> Update(T entity);
+        Task<T> Delete(T entity);
+    }
+}
---
+++ HR_Management.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HR_Management.Domain.Common
+{
+    public class BaseDomainEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
