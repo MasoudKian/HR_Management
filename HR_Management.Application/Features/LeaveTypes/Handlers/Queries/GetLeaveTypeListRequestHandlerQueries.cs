3b9f741ef7c3783ea99674a0d6d1d1e56566be0a
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-10 10:24:31 +0330

    Start Part-15

---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Queries/GetLeaveTypeListRequestHandlerQueries.cs
@@ -0,0 +1,19 @@
+﻿using HR_Management.Application.DTOs;
+using HR_Management.Application.Features.LeaveTypes.Requests;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Queries
+{
+    public class GetLeaveTypeListRequestHandlerQueries :
+        IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypeDTO>>
+    {
+        public Task<List<LeaveTypeDTO>> Handle(GetLeaveTypeListRequest request
+            , CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ HR_Management.Application/Features/LeaveTypes/Requests/GetLeaveTypeListRequest.cs
@@ -0,0 +1,10 @@
+﻿using HR_Management.Application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace HR_Management.Application.Features.LeaveTypes.Requests
+{
+    public class GetLeaveTypeListRequest : IRequest<List<LeaveTypeDTO>>
+    {
+    }
+}
