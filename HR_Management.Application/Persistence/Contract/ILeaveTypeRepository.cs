262cc166689618ca7941f6020d064e922dadbf8c
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-10 09:10:53 +0330

    Start Part-10

---
+++ HR_Management.Application/Persistence/Contract/ILeaveTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿using HR_Management.Domain;
+
+namespace HR_Management.Application.Persistence.Contract
+{
+    public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+    {
+    }
+}
