d3ecf60a81d41300d3edad6264a694a63f3c0da6
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-10 09:32:34 +0330

    Start Part-12

---
+++ HR_Management.Application/DTOs/LeaveAllocationDTO.cs
@@ -0,0 +1,12 @@
+﻿using HR_Management.Application.DTOs.Common;
+
+namespace HR_Management.Application.DTOs
+{
+    public class LeaveAllocationDTO : BaseDTO
+    {
+        public int NumberOfDays { get; set; }
+        public LeaveTypeDTO LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Priod { get; set; }
+    }
+}
--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -1,9 +1,16 @@
 ﻿using AutoMapper;
+using HR_Management.Application.DTOs;
+using HR_Management.Domain;
 
 namespace HR_Management.Application.Profiles
 {
     public class MappingProfile : Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest,LeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+        }
     }
 }
