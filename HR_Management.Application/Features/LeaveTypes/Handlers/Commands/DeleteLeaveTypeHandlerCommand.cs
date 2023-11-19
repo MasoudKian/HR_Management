9fbb30d524f8617535c421c7ad86190751822206
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-19 10:06:47 +0330

    Delete LeaveType

---
+++ HR_Management.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeHandlerCommand.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR_Management.Application.Features.LeaveTypes.Requests.Commands;
+using HR_Management.Application.Persistence.Contract;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR_Management.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeHandlerCommand : IRequestHandler<DeleteLeaveTypeRequestCommand, Unit>
+    {
+        #region Constructor
+
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypeHandlerCommand(ILeaveTypeRepository leaveTypeRepository
+            , IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        #endregion
+        public async Task<Unit> Handle(DeleteLeaveTypeRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+            await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
