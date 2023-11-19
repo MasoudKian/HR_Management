675ec4c6e1683acd8519fd097cb4f2bb5c421e61
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-19 08:58:17 +0330

    Start-P22

---
+++ HR_Management.Application/DTOs/LeaveRequest/ChangeLeaveRequestApprovalDTO.cs
@@ -0,0 +1,9 @@
+﻿using HR_Management.Application.DTOs.Common;
+
+namespace HR_Management.Application.DTOs.LeaveRequest
+{
+    public class ChangeLeaveRequestApprovalDTO : BaseDTO
+    {
+        public bool? Aoorived { get; set; }
+    }
+}
