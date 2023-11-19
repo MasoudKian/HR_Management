628cdc86de87a691c40d18b60f3e39c5736d8c95
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-19 09:04:46 +0330

    Set UpdateCommand

---
+++ HR_Management.Application/Features/LeaveRequests/Requests/Comands/UpdateLeaveRequestsRequestCommand.cs
@@ -0,0 +1,10 @@
+﻿using HR_Management.Application.DTOs.LeaveRequest;
+using MediatR;
+
+namespace HR_Management.Application.Features.LeaveRequests.Requests.Comands
+{
+    public class UpdateLeaveRequestsRequestCommand : IRequest<Unit>
+    {
+        public LeaveRequestDTO LeaveRequestDTO { get; set; }
+    }
+}
