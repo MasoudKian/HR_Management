4e32b2ff1b7ca5a82e3afb96ba16f880a9db0971
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-21 18:51:01 +0330

    Start P-40 -- On Model Creating

--- HR_Management.Persistence/LeaveManagementDbContext.cs
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -1,5 +1,9 @@
 ﻿using HR_Management.Domain;
+using HR_Management.Domain.Common;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR_Management.Persistence
 {
@@ -15,6 +19,45 @@ namespace HR_Management.Persistence
         public DbSet<LeaveType> LeaveTypes { get; set; }
         public DbSet<LeaveRequest> LeaveRequests { get; set; }
         public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        #endregion
+
+        #region On Model Creatiing
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(LeaveManagementDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess
+            , CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainEntity>())
+            {
+                entry.Entity.LastModifiedDate = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChanges();
+        }
 
         #endregion
     }
