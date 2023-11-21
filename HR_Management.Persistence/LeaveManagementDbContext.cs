6d48c85d31d434db8bd46dba9d871200452d4868
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-21 18:25:52 +0330

    Start P-39

---
+++ HR_Management.Persistence/LeaveManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HR_Management.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace HR_Management.Persistence
+{
+    public class LeaveManagementDbContext : DbContext
+    {
+        public LeaveManagementDbContext(DbContextOptions<LeaveManagementDbContext> options ) : base( options ) 
+        {
+            
+        }
+
+        #region DbSets
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+        #endregion
+    }
+}
