d6eb3d4095d67835d26a99e833484abdb9a20b3f
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-21 17:56:28 +0330

    Start P-37 -- Infrastructure

--- HR_Management.Application/Profiles/MappingProfile.cs
+++ HR_Management.Application/Profiles/MappingProfile.cs
@@ -10,10 +10,30 @@ namespace HR_Management.Application.Profiles
     {
         public MappingProfile()
         {
-            CreateMap<LeaveRequest,LeaveRequestDTO>().ReverseMap();
-            CreateMap<LeaveRequest,LeaveRequestListDTO>().ReverseMap();
+            #region LeaveRequest Mapping
+
+            CreateMap<LeaveRequest, LeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDTO>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDTO>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDTO>().ReverseMap();
+
+            #endregion
+
+            #region LeaveAllocation Mapping
+
             CreateMap<LeaveAllocation, LeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDTO>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDTO>().ReverseMap();
+
+            #endregion
+
+            #region LeaveType Mapping
+
             CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDTO>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDTO>().ReverseMap();
+
+            #endregion
         }
     }
 }
