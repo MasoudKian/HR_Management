fe15427340e39099ce5d0bbffa9232f9f6ea40b2
Masoud Kian <masoudkiannezhad@gmail.com>
2023-11-27 09:39:20 +0330

    PersistenceServicesRegistration Is Done -- S.P43

---
+++ HR_Management.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,29 @@
+﻿using HR_Management.Application.Persistence.Contract;
+using HR_Management.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR_Management.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services
+            , IConfiguration configuration)
+        {
+            services.AddDbContext<LeaveManagementDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("LeaveManagementConnectionString"));
+            });
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+            services.AddScoped<ILeaveAllocationRepostiory,LeaveAllocationRepostiory>();
+            services.AddScoped<ILeaveTypeRepository,LeaveTypeRepository>();
+            services.AddScoped<ILeaveRequestRepository,LeaveRequestRepository>();
+
+
+            return services;
+        }
+    }
+}
